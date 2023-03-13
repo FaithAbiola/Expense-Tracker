fec2cabebedbb8ab0b8533fe45ec063e3c522c4e
FaithAbiola <abiolafaith92@gmail.com>
2023-03-13 19:17:01 +0100

    Added more components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import Expenses from "./components/Expenses";
-
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 function App() {
     const expenses = [
         {
@@ -8,7 +8,11 @@ function App() {
             amount: 94.12,
             date: new Date(2020, 7, 14),
         },
-        {id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12)},
+        {
+            id: 'e2',
+            title: 'New TV',
+            amount: 799.49,
+            date: new Date(2021, 2, 12)},
         {
             id: 'e3',
             title: 'Car Insurance',
@@ -24,7 +28,7 @@ function App() {
     ];
     return (
         <div>
-            <h2>Let's get started</h2>
+            <NewExpense />
             <Expenses items={expenses}/>
         </div>
     );
